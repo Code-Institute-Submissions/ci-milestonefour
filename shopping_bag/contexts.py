ba0f08141e7c793e2fee262f51616be288fcea64
Matt Boland <mattjamesboland@gmail.com>
2020-11-11 17:33:36 +0000

    Finished products sizes functionality

--- shopping_bag/contexts.py
+++ shopping_bag/contexts.py
@@ -11,15 +11,27 @@ def shopping_bag_contents(request):
     product_count = 0
     shopping_bag = request.session.get('shopping_bag', {})
 
-    for item_id, quantity in shopping_bag.items():
-        product = get_object_or_404(Product, pk=item_id)
-        total += quantity * product.price
-        product_count += quantity
-        shopping_bag_items.append({
-            'item_id': item_id,
-            'quantity': quantity,
-            'product': product,
-        })
+    for item_id, item_data in shopping_bag.items():
+        if isinstance(item_data, int):
+            product = get_object_or_404(Product, pk=item_id)
+            total += item_data * product.price
+            product_count += item_data
+            shopping_bag_items.append({
+                'item_id': item_id,
+                'quantity': item_data,
+                'product': product,
+            })
+        else:
+            product = get_object_or_404(Product, pk=item_id)
+            for size, quantity in item_data['items_by_size'].items():
+                total += quantity * product.price
+                product_count += quantity
+                shopping_bag_items.append({
+                    'item_id': item_id,
+                    'quantity': item_data,
+                    'product': product,
+                    'size': size,
+                })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
