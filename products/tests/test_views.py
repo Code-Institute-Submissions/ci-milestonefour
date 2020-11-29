b54368d541483db7753c29ad5eec07f024bddc32
Matt Boland <mattjamesboland@gmail.com>
2020-11-29 14:25:32 +0000

    Temporarily add some login code to test products... not working

--- products/tests/test_views.py
+++ products/tests/test_views.py
@@ -20,13 +20,12 @@ class ProductViewTests(TestCase):
 
     def setUp(self):
         self.credentials = {
-            'username': 'testuser',
-            'password': 'secret'}
-        User.objects.create_user(**self.credentials)
-        
+            'username': 'temp',
+            'password': 'test'}
+
     def test_login(self):
         # send login data
-        response = self.client.post('/login/', self.credentials, follow=True)
+        response = self.client.post('/accounts/login/', self.credentials, follow=True)
         # should be logged in now
         self.assertTrue(response.context['user'].is_active)
 
