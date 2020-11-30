e55fce5a0aff666befbd43dc60c2b72d44a5d6ea
Matt Boland <mattjamesboland@gmail.com>
2020-11-30 23:28:19 +0000

    Remove failing tests as instructed by Guido, we couldnt figure out why they are failing

--- products/tests/test_views.py
+++ products/tests/test_views.py
@@ -20,38 +20,3 @@ class ProductViewTests(TestCase):
         response = self.client.post("/products/add/", {"pk": 101, "model": "products.product", "fields": {"sku": "9876", "name": "ADD PRODUCT", "description": "TEST", "has_sizes": "True", "price": 10.99, "category": 1, "subscription": "False", "rating": 5, "image_url": "", "image": ""}}, follow=True)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "account/login.html")
-
-    # Add New Product
-    def test_create_new_product(self):
-        # Needs login, but this doesn't seem to be working...
-        self.client.login(username='mattjboland', password='6Lb9MHC8zw')
-        response = self.client.post("/products/add/", {"pk": 101, "model": "products.product", "fields": {"sku": "9876", "name": "ADD PRODUCT", "description": "TEST", "has_sizes": "True", "price": 10.99, "category": 1, "subscription": "False", "rating":5, "image_url": "", "image": ""}}, follow=True)
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, "products/product_detail.html")
-
-    # New Product Details
-    def test_retrieve_new_product(self):
-        # Needs login, but this doesn't seem to be working...
-        self.client.login(username='mattjboland', password='6Lb9MHC8zw')
-        response = self.client.get("/products/101/")
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, "products/product_detail.html")
-
-    # Edit New Product
-    def test_edit_new_product(self):
-        # Needs login, but this doesn't seem to be working...
-        self.client.login(username='mattjboland', password='6Lb9MHC8zw')
-        response = self.client.get("/products/edit/101/")
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, "products/edit_product.html")
-        response = self.client.post("/products/edit/101/", {"pk": 101, "model": "products.product", "fields": {"sku": "9876", "name": "CHANGE PRODUCT", "description": "TEST", "has_sizes": "True", "price": 10.99, "category": 1, "subscription": "False", "rating": 5, "image_url": "", "image": ""}}, follow=True)
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, "products/product_detail.html")
-
-    # Delete Product
-    def test_delete_new_product(self):
-        # Needs login, but this doesn't seem to be working...
-        self.client.login(username='mattjboland', password='6Lb9MHC8zw')
-        response = self.client.get("/products/delete/101/", follow=True)
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, "products/products.html")
